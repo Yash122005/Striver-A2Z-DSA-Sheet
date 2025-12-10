e2f2c98cfa336b72011654d43fc48d61ac886c04
Yash Gupta <btech10430.24@bitmesra.ac.in>
2025-12-10 15:21:18 +0530

    Uncomment and fix count_occurrences and main logic

--- Step 01- Basic/Hashing/hash.cpp
+++ Step 01- Basic/Hashing/hash.cpp
@@ -2,64 +2,63 @@
 
 using namespace std;
 
-// int count_occurrences(int n, int arr[], int target)
-// {
-//     int cnt = 0;
-//     for (int i = 0; i < n; i++)
-//     {
-//         if (arr[i] == target)
-//         {
-//             cnt += 1;
-//         }
-//     }
-//     return cnt;
-// }
+int count_occurrences(int n, int arr[], int target)
+{
+    int cnt = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == target)
+        {
+            cnt += 1;
+        }
+    }
+    return cnt;
+}
 
 int main()
 {
-        // ****
-    // int n;
-    // cin >> n;
-    // int arr[n];
-    // for (int i = 0; i < n; i++)
-    //     cin >> arr[i];
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
     // prob 1
     // precompute
-    // int hash[13] = {0};
-    // for (int i = 0; i < n; i++)
-    // {
-    //     hash[arr[i]] += 1;
-    // }
-    // int q;               //no. of digits whose frequency you wants to know
-    // cin >> q;
-    // while (q--)
-    // {
-    //     int number;
-    //     cin >> number;
+    int hash[13] = {0};
+    for (int i = 0; i < n; i++)
+    {
+        hash[arr[i]] += 1;
+    }
+    int q;               //no. of digits whose frequency you wants to know
+    cin >> q;
+    while (q--)
+    {
+        int number;
+        cin >> number;
 
-    //     // fetch
-    //     cout << hash[number] << endl;
-    // }
+        // fetch
+        cout << hash[number] << endl;
+    }
 
-    // cout << count_occurrences(n, arr, target);
+    cout << count_occurrences(n, arr, target);
 
     // ****
 
-    // prob 2
-    // string s;
-    // cin>>s;
-    // // precompute
-    // int hash[26]={0};
-    // for(int i=0; i<s.size(); i++) hash[s[i]-'A']++;
+    prob 2
+    string s;
+    cin>>s;
+    // precompute
+    int hash[26]={0};
+    for(int i=0; i<s.size(); i++) hash[s[i]-'A']++;
 
-    // int q;
-    // cin>>q;
-    // while(q--){
-        // char c;         // c-> target characters
-    //     cin>>c;
-    //     // fetch
-    //     cout<<hash[c-'a']<<endl;
-    // }
+    int q;
+    cin>>q;
+    while(q--){
+        char c;         // c-> target characters
+        cin>>c;
+        // fetch
+        cout<<hash[c-'a']<<endl;
+    }
 
 
     //* map
